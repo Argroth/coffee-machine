bf83c69ac6c3f61d10fa9c80d0c988b61c690850
Argroth <lgronczakiewicz@gmail.com>
2022-04-06 13:30:31 +0200

    Working on coffee machine. Checking stocks, preparing etc.

--- Coffee-machine.py
+++ Coffee-machine.py
@@ -1,14 +1,68 @@
 class CoffeeMachine:
-   def __init__(self, coffee, milk, water):
-    self.coffee = coffee
-    self.milk = milk
-    self.water = water
+    def __init__(self, coffee, milk, water, money):
+        self.coffee = coffee
+        self.milk = milk
+        self.water = water
+        self.money = money
 
+    #print status of current resources
     def printStatus(self):
-        print("Coffee left: ", self.coffee)
-        print("Milk left: ", self.milk)
-        print("Water left: ", self.water)
+        print(f"Coffee left: {self.coffee}")
+        print(f"Milk left: {self.milk}")
+        print(f"Water left: {self.water}")
 
-coffeeMachine = CoffeeMachine("100", "500", "500")
+    #check remaining stocks before making a coffee
+    def checkRemainingStock(self, coffeToMake):
+        #get coffee to check ingredients
+        coffeIngredients = coffeeMenu[coffeToMake]
 
-print(coffeeMachine.printStatus())
+        if(coffeIngredients["water"] < self.water or coffeIngredients["coffee"] < self.coffee or coffeIngredients["milk"] < self.milk):
+            print("Za mało składników!")
+        # print(coffeIngredients)
+        print(f"resources left coffee: {self.coffee}, milk: {self.milk}, water: {self.water}")
+
+    #make coffee
+    def makeCoffee(self, coffeeToMake):
+        print(f"Making coffee: {coffeeToMake}")
+
+#Define coffee machine menu
+coffeeMenu = {
+    "latte": {
+        "water": 50,
+        "coffee": 20,
+        "milk": 150,
+        "price": 1.4
+    },
+
+    "cappucino": {
+        "water": 50,
+        "coffee": 20,
+        "milk": 150,
+        "price": 1.2
+    },
+
+    "black": {
+        "water": 50,
+        "coffee": 20,
+        "milk": 150,
+        "price": 0.8
+    }
+}
+
+#create new instance of Coffee Machine: coffee, milk, water, money
+coffeeMachine = CoffeeMachine(10, 500, 500, 5)
+
+print(coffeeMenu)
+x = input("Podaj co robić: ")
+if(x == "stock"):
+    coffeeMachine.printStatus()
+elif(x == "coffee"):
+    coffeToMake = input("Jaką kawę zrobić? ")
+    coffeeMachine.checkRemainingStock(coffeToMake)
+    # coffeeMachine.makeCoffee(coffeToMake)
+
+#
+#
+# coffeeMachine.printStatus()
+#
+# coffeeMachine.makeCoffee("Cappucino")
