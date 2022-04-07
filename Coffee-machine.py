f0ed58e8c44fa1002b43cd7c234066a1c4a8740d
argroth <basic15pl@gmail.com>
2022-04-07 19:52:02 +0200

    Working on machine options

--- Coffee-machine.py
+++ Coffee-machine.py
@@ -1,16 +1,5 @@
-import os
 import time
-import pymongo
 from tqdm import tqdm
-from dotenv import load_dotenv
-load_dotenv()
-
-# print(os.environ.get('DB_URL'))
-
-# DB config
-myClient = pymongo.MongoClient("mongodb://localhost:27017/")
-dbName = myClient["CoffeeMachine"]
-dbCoffeeMenu = dbName["CoffeeMenu"]
 
 
 class CoffeeMachine:
@@ -25,17 +14,15 @@ class CoffeeMachine:
 
     # print status of current resources
     def printStatus(self):
-        print(f"Income: {self.income}")
-        print(f"Coffees Made: {self.coffeesMade}")
         print(f"Coffee left: {self.coffee}")
         print(f"Milk left: {self.milk}")
         print(f"Water left: {self.water}")
         print(f"Money left: {self.money}")
 
     # check remaining stocks before making a coffee
-    def checkRemainingStock(self, coffeeToMake):
+    def checkRemainingStock(self, coffee):
         # get coffee to check ingredients
-        coffeeIngredients = coffeeMenu[coffeeToMake]
+        coffeeIngredients = coffeeMenu[coffee]
 
         # checking ingredients and printing results if there is not enough resources in stock
         if coffeeIngredients["water"] > self.water or coffeeIngredients["coffee"] > self.coffee or \
@@ -45,8 +32,8 @@ class CoffeeMachine:
         return False
 
     # make coffee
-    def makeCoffee(self, coffeeToMake, insertedCoin):
-        currentCoffee = coffeeMenu[coffeeToMake]
+    def makeCoffee(self, coffee, insertedCoin):
+        currentCoffee = coffeeMenu[coffee]
         self.coffee = self.coffee - currentCoffee['coffee']
         self.water = self.water - currentCoffee['water']
         self.milk = self.milk - currentCoffee['milk']
@@ -55,10 +42,9 @@ class CoffeeMachine:
 
         self.calculateIncome(currentCoffee['price'] - currentCoffee['cost'])
 
-        print(f"Making coffee: {coffeeToMake}")
+        print(f"Making coffee: {coffee}")
         for i in tqdm(range(100)):
-            time.sleep(0.001)
-            # time.sleep(0.08)
+            time.sleep(0.008)
 
         print(f"Price: {currentCoffee['price']}")
         print(f"Change: {insertedCoin - currentCoffee['price']}")
@@ -67,68 +53,59 @@ class CoffeeMachine:
         self.income = self.income + round(income, 3)
 
 
-def createNewCoffee(name, coffee, water, milk, price, cost):
-    dbCoffeeMenu.insert_one({name: {"water": water, "coffee": coffee, "milk": milk, "price": price, "cost": cost}})
+coffeeMenu = {
+    "black": {
+        "water": 2000,
+        "coffee": 1,
+        "milk": 1,
+        "price": 1.4,
+        "cost": 0.7
+    },
 
+    "cappuccino": {
+        "water": 50,
+        "coffee": 20,
+        "milk": 150,
+        "price": 1.2,
+        "cost": 0.7
+    },
 
-# Define coffee machine menu
-# coffeeMenu = {
-#     "black": {
-#         "water": 2000,
-#         "coffee": 1,
-#         "milk": 1,
-#         "price": 1.4,
-#         "cost": 0.7
-#     },
-#
-#     "cappuccino": {
-#         "water": 50,
-#         "coffee": 20,
-#         "milk": 150,
-#         "price": 1.2,
-#         "cost": 0.7
-#     },
-#
-#     "latte": {
-#         "water": 50,
-#         "coffee": 20,
-#         "milk": 150,
-#         "price": 1.4,
-#         "cost": 0.4
-#     }
-# }
+    "latte": {
+        "water": 50,
+        "coffee": 20,
+        "milk": 150,
+        "price": 1.4,
+        "cost": 0.4
+    }
+}
 
-x = dbCoffeeMenu.find({'latte': {'$exists': 1}})
+coffeeMachine = CoffeeMachine(1000, 500, 500, 10)
 
-print(list(x))
+answer = True
+while answer:
+    print("""
+    1.  Make a coffee
+    2.  Check Status
+    3.  Collect Income
+    Q.  Quit
+    """)
+    answer = input("Choose option: ")
+    if answer == "1":
+        moneyInserted = int(input("Please insert money! "))
+        coffeeToMake = input("What do you want to drink? ")
+        if not coffeeMachine.checkRemainingStock(coffeeToMake):
+            coffeeMachine.makeCoffee(coffeeToMake, moneyInserted)
 
+    elif answer == "2":
+        print(coffeeMachine.printStatus())
 
-# createNewCoffee("espresso", 150, 100, 0, 1, 0.5)
-# createNewCoffee("latte", 50, 20, 150, 1.4, 0.4)
-# createNewCoffee("black", 150, 20, 0, 1.2, 0.7)
-# createNewCoffee("cappuccino", 150, 100, 0, 1.4, 0.4)
-# coffeeMachine = CoffeeMachine(1000, 500, 500, 10)
-# #
-# # # x = input("Podaj co robić: ")
-# x = "coffee"
-# y = "latte"
-# z = 4
-# if x == "stock":
-#     coffeeMachine.printStatus()
-# elif x == "coffee":
-#     # coffeeToMake = input("Jaką kawę zrobić? ")
-#     if not coffeeMachine.checkRemainingStock(y):
-#         coffeeMachine.makeCoffee(y, z)
-#         coffeeMachine.printStatus()
+    elif answer == "3":
+        print("\n Collecting income...")
+        print(f"Collected income: {coffeeMachine.income}")
+        coffeeMachine.income = 0
 
-# TODO add menu
-# TODO add alerts for low stocks
-# TODO add income + tax
-# TODO calculate effectiveness
-# TODO create report
-# TODO add stock to instance
-# TODO add collect income
-# TODO add progress bar
-# coffeeMachine.printStatus()
-#
-# coffeeMachine.makeCoffee("Cappucino")
+    elif answer == "Q" or answer == "q":
+        print("\n Goodbye")
+        answer = None
+    else:
+        print("\n Not Valid Choice Try again")
